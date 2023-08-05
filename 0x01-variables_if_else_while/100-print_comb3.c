e7c8253765821addd60f672ac56477f038f055d5
Hafsamha <Hafsamha@gmail.com>
2023-08-04 20:07:49 -0700

    0x0A. C - argc, argv

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, x;
+	{
+
+	for (n = '0'; n < '9'; n++)
+		putchar('n');
+	
+	{
+	for (x = n + 1; x <= '9'; x++)
+			putchar('x');
+	
+	{
+		if (x != n)
+			if (n == '8' && x == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
+	}
+	}
+	}
+	
+			putchar('\n');
+			return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-alx-low_level_programming
+alx-low level programming
+C - argc, argv
+
